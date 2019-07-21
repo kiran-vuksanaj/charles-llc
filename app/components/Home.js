3cc7adbeea9ba7b77b971e33d036447e20e68f47
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-07-21 15:03:03 -0400

    testing commit

--- app/components/Home.js
+++ app/components/Home.js
@@ -13,6 +13,7 @@ export default class Home extends Component<Props> {
     return (
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
+        <h3>Kirans repo</h3>
         <Link to={routes.COUNTER}>to Counter</Link>
       </div>
     );
